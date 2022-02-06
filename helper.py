f0d963503db3d9acff73e7986f8b24092421855e
Sykes368 <945mitchelsykes@gmail.com>
2022-02-05 22:39:03 -0500

    Init

---
+++ helper.py
@@ -0,0 +1,16 @@
+# Color codes & quick access to error, and warning message prefixes
+class Text:
+    # Colors
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    GRAY = '\033[37m'
+    # Formatting + Reset all
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    # Prefixes
+    ERROR = f'{GRAY}[{RED}!!{GRAY}]{RESET}'
+    WARN = f'{GRAY}[{YELLOW}!{GRAY}]{RESET}'
