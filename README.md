e76d41c0bba6150dd182cc44d5ea6e8c8fa3b5c3
Sykes368 <945mitchelsykes@gmail.com>
2022-03-18 19:34:51 -0400

    Midterm : Designed for Linux

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Python2
+# Python2 Midterm
+This script was designed for linux systems. It possibly run on other unix-based operating systems.
--- main.py
+++ main.py
@@ -1,37 +1,14 @@
-from scapy.all import *
 from helper import Text
-import re
-
-# Sniffs X packets and prints/saves info about each packet
-def sniffer(num, save,filename):
-    capture = sniff(filter='ip',prn=lambda s: (s.sprintf(f"Time: {Text.BLUE}%IP.time%{Text.RESET}\nProtocol: {Text.BLUE}%IP.proto%{Text.RESET}\nPort: {Text.BLUE}%IP.sport%{Text.RESET} \nSource IP: {Text.BLUE}%IP.src%{Text.RESET} \nDestination IP: {Text.BLUE}%IP.dst%{Text.RESET} \nFlags: {Text.BLUE}%IP.flags%{Text.RESET} \nData: {Text.BLUE}%Raw.load%{Text.RESET}\n")), count=num)
-
-    # Writes output to pcap
-    if save:
-        wrpcap(f"{filename}.pcap", capture)
-    
-    # Prints Capture Summary
-    print(capture)
+import scanner
 
 
-# Gets User Input & Runs sniffer()
+# Checks compatibility, gets all connected networks and run an arp scan on each network
 if __name__ == '__main__':
-    print("Python Packet Sniffer!\n")
-   
-    num = input("Number of packets to capture [default: 10]: ") 
-    if not re.match("^[0-9]+", num):
-        num = 10
-    
-    save = input("Save output to pcap [y/N]? ")
+    scanner.check_os_and_permissions()
 
-    # Checks input and verifies pcap filename
-    if re.match("y", save) or re.match("Y", save): 
-        save = True
-        filename = input("Pcap filename [default: out]: ")
-        if filename==None or filename=="":
-            filename="out"
-    else:
-        save = False
-        filename=""
+    networks = scanner.get_networks()
+    print(f"{Text.INFO}This device is connected to {Text.BLUE}{len(networks)}{Text.RESET} network(s): {Text.BLUE}{networks}{Text.RESET}")
 
-    sniffer(int(num), save, filename)
+    for network in networks:
+        print(f"{Text.INFO}ARP Scanning the {network} network.")
+        scanner.arp_scan(network)
