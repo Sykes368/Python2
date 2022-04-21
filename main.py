e5740e1da7207a3a3a7fbde2a61b7e88339faf01
Sykes368 <945mitchelsykes@gmail.com>
2022-04-21 00:11:44 -0400

    Assignment 7

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from helper import Text
-from browser import *
+import bs4
+import requests
+import re
 
 
-# Gets url from user, lanuches firefox,  then navigates to the url
+# From the blackhat websites gets all presenters from blackhat USA 2000
 if __name__ == '__main__':
-    print(f"{Text.INFO}This script requires that {Text.BLUE}geckodriver{Text.RESET} is in the PATH")
-   
-    url = get_url()
-    firefox_instance = lanuch_firefox()
-    navigate_to(firefox_instance, url)
+    # Temporarlly Downloads the website to parse
+    res = requests.get("https://www.blackhat.com/html/bh-media-archives/bh-archives-2000.html")
+    res.raise_for_status()
+    
+    # Gets the list of presentations from the webpage
+    blackhat = bs4.BeautifulSoup(res.text, 'html.parser')
+    presentations = blackhat.select('a[title="Black Hat USA 2000"][href]')
+    
+    # Gets the presenter names from the list of presentations
+    presenters = re.findall(r'([A-Za-z\.]+ *[A-Za-z\.]* *[A-Za-x]*)<', str(presentations)) + re.findall(r'([A-Za-z\.]+ *[A-Za-z\.]* *[A-Za-x]*) \&amp', str(presentations))
+
+    # Prints out the the presenters
+    print(f"BlackHat USA 2000 Presenters: ")
+    for presenter in presenters:
+        print(f"-{Text.BLUE}{presenter}{Text.RESET}")
