7bc17533454ff64594f8fdaa9f24aa10f0c29074
Sykes368 <945mitchelsykes@gmail.com>
2022-05-20 13:57:55 -0400

    Updated

--- main.py
+++ main.py
@@ -2,22 +2,38 @@ from helper import Text
 import bs4
 import requests
 import re
+import sys
+
+
+# Temporarlly Downloads the blackhat website to parse
+def get_blackhat_html():
+    try:
+        res = requests.get("https://www.blackhat.com/html/bh-media-archives/bh-archives-2000.html")
+        res.raise_for_status()
+    except:
+        print(f"{Text.ERROR}Error Downloading Blackhat website. Exiting...")
+        sys.exit()
+    
+    return res
+
+
+# From the html from the blackhat site parses out the presenters
+def get_presenters(bh_html):
+    # Gets the list of presentations from the webpage
+    try:        
+        blackhat = bs4.BeautifulSoup(bh_html.text, 'html.parser')
+        presentations = blackhat.select('a[title="Black Hat USA 2000"][href]')
+    except:
+        print(f"{Text.ERROR}Error Parsing Blackhat website. Exiting...")
+        sys.exit()
+
+    # Returns the presenter names from the list of presentations
+    return re.findall(r'([A-Za-z\.]+ *[A-Za-z\.]* *[A-Za-x]*)<', str(presentations)) + re.findall(r'([A-Za-z\.]+ *[A-Za-z\.]* *[A-Za-x]*) \&amp', str(presentations))
 
 
 # From the blackhat websites gets all presenters from blackhat USA 2000
 if __name__ == '__main__':
-    # Temporarlly Downloads the website to parse
-    res = requests.get("https://www.blackhat.com/html/bh-media-archives/bh-archives-2000.html")
-    res.raise_for_status()
-    
-    # Gets the list of presentations from the webpage
-    blackhat = bs4.BeautifulSoup(res.text, 'html.parser')
-    presentations = blackhat.select('a[title="Black Hat USA 2000"][href]')
-    
-    # Gets the presenter names from the list of presentations
-    presenters = re.findall(r'([A-Za-z\.]+ *[A-Za-z\.]* *[A-Za-x]*)<', str(presentations)) + re.findall(r'([A-Za-z\.]+ *[A-Za-z\.]* *[A-Za-x]*) \&amp', str(presentations))
-
-    # Prints out the the presenters
+     # Prints out the the presenters from blackhat USA 200
     print(f"BlackHat USA 2000 Presenters: ")
-    for presenter in presenters:
+    for presenter in get_presenters(get_blackhat_html()):
         print(f"-{Text.BLUE}{presenter}{Text.RESET}")
