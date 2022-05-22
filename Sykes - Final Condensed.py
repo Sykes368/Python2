3cc63e436626162318078288a26fec1c0da4ea8f
Sykes368 <945mitchelsykes@gmail.com>
2022-05-22 17:24:41 -0400

    Final

--- Sykes - Final Condensed.py
+++ Sykes - Final Condensed.py
@@ -1,8 +1,28 @@
-from helper import Text
 import os.path as path
 import socket
 import sys
 import ssl
+
+
+## Contents of helper.py added here to condense to 1 file
+# Color codes & quick access to error, and warning message prefixes
+class Text:
+    # Colors
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    GRAY = '\033[37m'
+    # Formatting + Reset all
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    # Prefixes
+    ERROR = f'{GRAY}[{RED}!!{GRAY}]{RESET} '
+    WARN = f'{GRAY}[{YELLOW}!{GRAY}]{RESET} '
+    INFO = f'{GRAY}[{BLUE}*{GRAY}]{RESET} '
+    SUCCESS = f'{GRAY}[{GREEN}*{GRAY}]{RESET} '
 
 
 # Returns a list of domains, that were read from the given file
@@ -21,7 +41,7 @@ def read_domains_txt(filename):
     return domains.readlines()
 
 
-# Returns a bool that is of whether or not the given domain has ssl
+# Returns a bool that is whether or not the given domain has ssl
 def check_ssl(domain):
         context = ssl.create_default_context()
         
